30e5f142cebc5941b2dad771b802595aebe9ba84
Piktorikusz <piktorikusz@gmail.com>
2020-11-23 02:08:01 +0100

    gombok

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var buttons = document.getElementsByTagName("button");
+var buttonsCount = buttons.length;
+for (var i = 0; i <= buttonsCount; i += 1) {
+    buttons[i].onclick = function(event) {
+        console.log(this.innerHTML);
+    };
+};
